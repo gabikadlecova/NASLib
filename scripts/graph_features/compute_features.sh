94616af30794c190010f35f3af8a71e48388f0a1
Gabriela Suchoparova <gabi.suchoparova@gmail.com>
2024-01-15 17:44:50 +0000

    Remove old dependencies. Fix graph features scripts - correct args and config calls.

--- scripts/graph_features/compute_features.sh
+++ scripts/graph_features/compute_features.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 
-out_dir=`basename $1`
+out_dir=$1
 dataset=$2
 search_space=$3
 
-graph_pred_args="--graph_features_config_path $2 --graph_features_path $3 --valid_networks $4"
+graph_pred_args="--graph_features_config_path $4 --graph_features_path $5 --valid_networks $6"
 
-python create_configs.py --predictor graph_features --experiment_type vary_train_size \
+python ../create_configs.py --predictor graph_features --experiment_type vary_train_size \
   --test_size 200 --start_seed 0 --trials 1 --out_dir $out_dir \
   --dataset=$dataset --config_type predictor --search_space $search_space \
   $graph_pred_args
 
-python save_graph_features.py --config_path $out_dir/$dataset/configs/predictors/config_graph_features_0.yaml \
-  --out_path $1
+python save_graph_features.py --config-file $out_dir/$dataset/configs/predictors/config_graph_features_0.yaml \
+  --out_path "$out_dir"$search_space-$dataset.pickle
 
 rm $out_dir/$dataset/configs/predictors/config_graph_features_0.yaml
+
--- scripts/graph_features/save_graph_features.py
+++ scripts/graph_features/save_graph_features.py
@@ -1,18 +1,17 @@
-import argparse
-
 from naslib.predictors.graph_features import GraphFeaturesPredictor
 from naslib import utils
+from naslib.utils import default_argument_parser
 
 
 def save_predictor(args):
     config = utils.get_config_from_args(args, config_type="predictor")
     pred = GraphFeaturesPredictor(config)
-    pred.save_dataset(args['out_path'])
+    print(args.out_path)
+    pred.save_dataset(args.out_path)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Save graph features')
+    parser = default_argument_parser()
     parser.add_argument('--out_path', type=str, help='Pickle path to save graph features')
-    parser.add_argument('--config_path', type=str, help='Pickle path to save graph features')
     args = parser.parse_args()
     save_predictor(args)
